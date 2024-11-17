17430c111fb5c12f220f9985a3096737c139a3ca
Tom G <tom@embeddable.com>
2024-11-17 17:29:37 +0100

    update header

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,14 +4,14 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>Embeddable Handbook</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/embeddable-hq',
   },
   chat: {
     link: 'https://discord.com',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Embeddable Handbook',
   },
 }
 
