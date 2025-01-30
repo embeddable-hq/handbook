c5b44b227e43ba920c1a1d24815e75310aa9a0bb
Henry Marshall <harry@embeddable.com>
2025-01-30 22:31:34 +0100

    New components added (cardgrid) Multiple sections updated New images and videos added

--- components/LinkCard.tsx
+++ components/LinkCard.tsx
@@ -6,19 +6,21 @@ interface LinkCardProps {
   href: string;
   icon?: React.ReactNode;
   children?: React.ReactNode;
+  marginTop?: string;
 }
 
-const LinkCard: React.FC<LinkCardProps> = ({ title, href, icon, children }) => {
+const LinkCard: React.FC<LinkCardProps> = ({ title, href, icon, children, marginTop = "1rem" }) => {
   return (
     <Link
       href={href}
-      style={{ marginTop: "2rem" }}
+      style={{ marginTop: marginTop }}
       className="nextra-card nx-group nx-flex nx-flex-col nx-justify-start nx-overflow-hidden nx-rounded-lg nx-border nx-border-gray-200 nx-text-current nx-no-underline dark:nx-shadow-none hover:nx-shadow-gray-100 dark:hover:nx-shadow-none nx-shadow-gray-100 active:nx-shadow-sm active:nx-shadow-gray-200 nx-transition-all nx-duration-200 hover:nx-border-gray-300 nx-bg-transparent nx-shadow-sm dark:nx-border-neutral-800 hover:nx-bg-slate-50 hover:nx-shadow-md dark:hover:nx-border-neutral-700 dark:hover:nx-bg-neutral-900"
     >
       <div className="flex items-start space-x-3">
-        {icon && <div className="text-xl">{icon}</div>}
+        
         <div>
           <h3 className="nx-flex nx-font-semibold nx-items-start nx-gap-2 nx-p-4 nx-text-gray-700 hover:nx-text-gray-900 dark:nx-text-neutral-200 dark:hover:nx-text-neutral-50 nx-flex nx-items-center">
+            {icon && <span className="text-xl">{icon}</span>}
             {title}
             <span>&#x2192;</span>
           </h3>
