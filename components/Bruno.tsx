91cc755615223586f3b06625426763fe4642832b
Tom Gardiner <tom@trevor.io>
2025-09-03 10:16:41 +0200

    add bruno

---
+++ components/Bruno.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const BrunoButton = () => {
+
+  return (
+    <a target='_blank' href='https://fetch.usebruno.com/?url=https%3A%2F%2Fgithub.com%2Fembeddable-hq%2Fembeddable-public-apis'>
+      <img src="https://fetch.usebruno.com/button.svg" alt="Fetch in Bruno" style={{ width: '160px', height: '40px', marginTop: '1.5rem' }} width="160" height="40"></img>
+    </a>
+  );
+};
+
+export default BrunoButton;
