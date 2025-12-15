931881cab88f4d10d174c67a0d6b0a39571fc29f
Christopher Buecheler <chris@embeddable.com>
2025-12-15 12:26:03 +0100

    Move to React-based component, fix issue w/ terminal error

--- components/GenerateDateModels.tsx
+++ components/GenerateDateModels.tsx
@@ -1,50 +1,53 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
+import {
+  InkeepEmbeddedChat,
+  type InkeepEmbeddedChatProps,
+} from "@inkeep/agents-ui";
 
 export default function GenerateDataModels() {
-  const { setTheme, resolvedTheme } = useTheme();
+  const [showChild, setShowChild] = useState(false);
+  const { resolvedTheme } = useTheme();
   const isDarkMode = resolvedTheme === "dark";
 
+  // We do this to avoid a useLayoutEffect warning
   useEffect(() => {
-    const config = {
-      baseSettings: {
-        apiKey: process.env.NEXT_PUBLIC_INKEEP_API_KEY!,
-        primaryBrandColor: "#6778DE",
-        colorMode: {
-          forcedColorMode: isDarkMode ? "dark" : "light",
-        },
-        aiAssistantAvatar:
-          "https://storage.googleapis.com/organization-image-assets/embeddable-botAvatarSrcUrl-1747040031545.png",
-      },
-      aiChatSettings: {
-        aiAssistantName: "Embeddable Model Generator",
-        introMessage:
-          "👋 Hi, Paste your SQL query below and I’ll turn it into a Cube.js model ⚡?",
-        exampleQuestions: [
-          "Can you help me convert my SQL into data models?",
-          "Can you help me convert my table ddl/schema into data models?",
-        ],
-      },
-    };
-    // @ts-expect-error -- Inkeep script adds this to window
-    if (window && window.Inkeep) {
-      // @ts-expect-error -- Inkeep script adds this to window
-      window.Inkeep?.EmbeddedChat("#ikp-embedded-chat-target", config);
-    }
-  }, [isDarkMode]);
+    setShowChild(true);
+  }, []);
 
-  return (
+  const embeddedChatProps: InkeepEmbeddedChatProps = {
+    baseSettings: {
+      primaryBrandColor: "#6778DE",
+      colorMode: {
+        forcedColorMode: isDarkMode ? "dark" : "light",
+      },
+    },
+    aiChatSettings: {
+      apiKey: process.env.NEXT_PUBLIC_INKEEP_API_KEY!,
+      aiAssistantAvatar:
+        "https://storage.googleapis.com/organization-image-assets/embeddable-botAvatarSrcUrl-1747040031545.png",
+      aiAssistantName: "Embeddable Model Generator",
+      introMessage:
+        "👋 Hi, Paste your SQL query below and I’ll turn it into a Cube.js model ⚡?",
+      exampleQuestions: [
+        "Can you help me convert my SQL into data models?",
+        "Can you help me convert my table ddl/schema into data models?",
+      ],
+    },
+  };
+
+  return showChild ? (
     <div
       style={{
         display: "flex",
-        alignItems: "center",
         justifyContent: "center",
         height: "calc(100vh - 16px)",
+        paddingTop: 30,
       }}
     >
       <div style={{ maxHeight: "600px", height: "100%", width: "100%" }}>
-        <div id="ikp-embedded-chat-target" style={{ width: "100%" }}></div>
+        <InkeepEmbeddedChat {...embeddedChatProps} />;
       </div>
     </div>
-  );
+  ) : null;
 }
